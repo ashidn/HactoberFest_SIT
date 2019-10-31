74849a1182828a84780b560a71b398dbbefc66f6
ashidn <57225331+ashidn@users.noreply.github.com>
2019-10-31 22:12:03 +0530

    Update reverse.c
    No need to use additional variables like flag.

--- HactoberFest_Programs/reverse.c
+++ HactoberFest_Programs/reverse.c
@@ -2,32 +2,20 @@
 #include<string.h>
 
 int main(){
-	int size=0,i,flag=1;
+	int size, i;
 	char str[50];
 	do{
-		
-	printf("\nEnter a string: ");
-	gets(str);
-	i=0;
-	while(str[i]!='\0'){
-		i++;
-	}
-	size=i-1;
-	for(i=0;i<=size/2;i++){
-		if(str[i]!=str[size-i]){
-			flag=0;
-			break;
+		printf("\nEnter a string: ");
+		gets(str);
+		size = strlen(str)-1;
+		for(i=0; i<=size/2; i++){
+			if(str[i] != str[size-i])
+				break;
+			printf("\n%c\t%c", str[i], str[size-i]);
 		}
-		else{
-			flag=1;
-		}
-		printf("\n%c\t%c",str[i],str[size-i]);
-	}
-	if(flag==1){
-		printf("\nPalindrome");
-	}
-	else{
-		printf("\nNot Palindrome");
-	}
-	}while(str!="end");
-}	
+		if(i > size/2)
+			printf("\nPalindrome");
+		else
+			printf("\nNot Palindrome");
+	}while(str != "end");
+}
